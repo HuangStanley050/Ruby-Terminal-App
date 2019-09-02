5dbea603f49ed477a3c05862b85ff55f397ce765
Stanley Huang <huang.stanley050@gmail.com>
2019-09-03 09:53:09 +1000

    somehow get the progress bar working while waiting for api response

--- testHttparty.rb
+++ testHttparty.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
 require 'httparty'
+require 'tty'
 
 CHUCK_NORRIS_URL = 'https://api.chucknorris.io/jokes/random'
+$bar = TTY::ProgressBar.new('downloading [:bar]', total: 50)
 
 def get_joke
-  response = HTTParty.get(CHUCK_NORRIS_URL)
-  hash = response.parsed_response
+  response = nil
+  httpStatus = nil
+  hash = nil
+
+  until httpStatus
+    response = HTTParty.get(CHUCK_NORRIS_URL)
+    httpStatus = response.code
+    hash = response.parsed_response
+    50.times do
+      sleep(0.1)
+      $bar.advance(1)
+    end
+  end
+
   puts hash['value']
+  puts httpStatus
 end
 
 get_joke
