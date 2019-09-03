f8df1268243ec00338ce4eb9683ac89c1b212ffa
Stanley Huang <huang.stanley050@gmail.com>
2019-09-04 09:34:56 +1000

    wrote two test cases for getJoke and saveJoke

---
+++ test/testChuckNorris.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rspec/autorun'
+
+require '../getJoke'
+require '../saveJoke'
+
+describe 'Testing for external API call' do
+  it 'should returns a string' do
+    test_string = get_joke
+    expect(test_string).to be_a(String)
+  end
+end
+
+describe 'Testing for creating/appending a file to a named folder' do
+  it 'current directory should contain a folder named "chuck_norris_jokes" and has a file inside' do
+    saveJoke
+    expect(File.directory?('chuck_norris_jokes')).to be true
+    is_directory_empty = Dir.empty?('chuck_norris_jokes')
+    expect(is_directory_empty).to be false
+  end
+end
