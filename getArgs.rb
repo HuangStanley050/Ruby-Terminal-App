744869f5883b3952bae4349434f06c2ac4740c37
Stanley Huang <huang.stanley050@gmail.com>
2019-09-03 14:40:27 +1000

    able to run just from command line with correct arguments passed to it

--- getArgs.rb
+++ getArgs.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
+require_relative 'getJoke.rb'
+require_relative 'saveJoke.rb'
+require 'colorize'
+
 def get_arguments
-  ARGV.each do |arg|
-    puts arg
+  # only do one argument for now, to get a random joke or to write to file of the random joke
+  argument = ARGV[0]
+  if argument != '-R' && argument != '-W'
+    puts 'Invalid argument passed to the app '
+    puts "you have typed invalid command: ruby #{$PROGRAM_NAME} #{argument}"
+    puts '=================================='
+    puts 'Example of usage: '
+    puts 'ruby main.rb -W'.yellow
+    puts 'ruby main.rb -R'.blue
+    puts '=================================='
   end
+  puts get_joke if argument == '-R'
+  puts saveJoke if argument == '-W'
 end
