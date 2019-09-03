e8a53124dc5740771adc36d7fa4104a79e27c6de
Stanley Huang <huang.stanley050@gmail.com>
2019-09-03 13:24:08 +1000

    able to create the folder and save the joke into it

--- saveJoke.rb
+++ saveJoke.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
 require 'httparty'
+require 'fileutils'
+require 'uuid'
+require 'date'
+
 CHUCK_NORRIS_URL = 'https://api.chucknorris.io/jokes/random'
 
 def saveJoke
   response = nil
   httpStatus = nil
   hash = nil
-
+  directoryName = 'chuck_norris_jokes'
   response = HTTParty.get(CHUCK_NORRIS_URL)
   hash = response.parsed_response
 
   fileContent = hash['value']
 
-  File.open('test.chuck', 'w') do |f|
+  Dir.mkdir(directoryName) unless File.exist?(directoryName)
+
+  File.open('./chuck_norris_jokes/test2.chuck', 'w') do |f|
     f.write(fileContent)
   end
 end
