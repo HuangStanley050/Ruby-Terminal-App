13f9448a142b2d2d4a5b7669fee1d5bea0d9f38f
Stanley Huang <huang.stanley050@gmail.com>
2019-09-03 13:56:45 +1000

    running into this error: initialize: Is a directory @ rb_sysopen

--- saveJoke.rb
+++ saveJoke.rb
@@ -2,7 +2,7 @@
 
 require 'httparty'
 require 'fileutils'
-require 'uuid'
+
 require 'date'
 
 CHUCK_NORRIS_URL = 'https://api.chucknorris.io/jokes/random'
@@ -11,15 +11,26 @@ def saveJoke
   response = nil
   httpStatus = nil
   hash = nil
+  fileName = ''
+
   directoryName = 'chuck_norris_jokes'
   response = HTTParty.get(CHUCK_NORRIS_URL)
   hash = response.parsed_response
 
   fileContent = hash['value']
 
+  d = DateTime.now
+  time = d.strftime('%a %d %b %Y')
+  randomString = rand(36**20).to_s(36)
+
+  filenName = time
+  puts fileName
+
   Dir.mkdir(directoryName) unless File.exist?(directoryName)
 
-  File.open('./chuck_norris_jokes/test2.chuck', 'w') do |f|
+  save_string = 'chuck_norris_jokes/' + fileName
+
+  File.open(save_string, 'w') do |f|
     f.write(fileContent)
   end
 end
